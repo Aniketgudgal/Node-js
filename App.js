ac041d4166b5fd3bf64a93e5278668a9793a232b
Aniketgudgal <aniketgudgal5867@gmail.com>
2026-04-08 21:38:51 +0530

    Create Simple server and print request

---
+++ App.js
@@ -0,0 +1,7 @@
+const http = require('http');
+const server = http.createServer((req, res) => {
+console.log(req);
+});
+
+const PORT = 3000;
+server.listen(PORT);
--- FirstProgram.js
+++ FirstProgram.js
@@ -1,7 +1,9 @@
 const http = require('http');
 const PORT = 3000;
 const server =  http.createServer((req, res) => {
-console.log(req);
+console.log(req.url);
+console.log(req.method);
+console.log(req.headers);
 res.write('<h1>Hello Gokul Kaka</h1>');
 res.write('<p>Hello world</p>');
 });
